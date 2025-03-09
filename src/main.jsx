b24e58a0b560efcf8b04ea3a9d6d400bacbd7368
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-03-09 14:37:24 +0000

    adding skill component , and passing porps to it in the skillList ,also destructring props to mike skill component more readble

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,25 @@
+
+import "./style.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./style.css";
+
 
  function App() {
   return (
-    <div className="card">
-      <Avatar />
+    <div className="card" >
+      <Avatar imgSrc="myImg.jpeg"/>
       <div className="data">
         <Intro
-          about="Hello, I‘m Mohamed Amine
-          Front-End React Developer
-          Passionate and driven Front-End Developer "
+          aboutMe="front-End Developer specializing in React.js , Beyond coding, I enjoy creating content about web development, sharing my knowledge, and helping others learn."
           name="Mohamed amine Mounir"
-        />
-     
-        <SkillList
-          red={{ backgroundColor: "red" }}
-          blue={{ backgroundColor: "blue" }}
-          yellow={{ backgroundColor: "yellow" }}
-          green={{ backgroundColor: "green" }}
-          orange={{ backgroundColor: "orange" }}
-          purple={{ backgroundColor: "purple" }}
-          emoji1="💪"
-          skill1="Html+CSS"
-          emoji2="💪"
-          skill2="JavaScript"
-          emoji3="👍"
-          skill3="React.Js"
-          emoji4="✨"
-          skill4="tailwind CSS"
-          emoji5="👍"
-          skill5="Git and Github"
-        />
+      />
+        <SkillList/>
       </div>
     </div>
   );
 }
+
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -46,40 +30,54 @@ root.render(
   </StrictMode>
 );
 
-function Avatar() {
-  return <img src="myImg.jpeg" alt="image" className="avatar" />;
+function Avatar({imgSrc}) {
+  return <img src={imgSrc} alt="image" className="avatar" />;
 }
-function Intro(props) {
+function Intro({name,aboutMe}) {
   return (
     <div>
-      <h1>{props.name}</h1>
-      <p>{props.about}</p>
+      <h1>{name}</h1>
+      <p>{aboutMe}</p>
     </div>
   );
 }
-function SkillList(props) {
+
+
+function SkillList() {
   return (
     <div className="skill-list">
-      <p className="skill" style={props.purple}>
-        {props.skill1}
-        {props.emoji1}
-      </p>
-      <p className="skill" style={props.green}>
-        {props.skill2}
-        {props.emoji2}
-      </p>{" "}
-      <p className="skill" style={props.blue}>
-        {props.skill3}
-        {props.emoji3}
-      </p>{" "}
-      <p className="skill" style={props.yellow}>
-        {props.skill4}
-        {props.emoji4}
-      </p>
-      <p className="skill" style={props.red}>
-        {props.skill5}
-        {props.emoji5}
-      </p>
+      <Skill    
+          emoji="💪"
+          skill="Html+CSS"
+          color={{ backgroundColor: "red" }}
+          />
+      <Skill    
+          emoji="💪"
+          skill="JavaScript"
+          color={{ backgroundColor: "blue" }}
+          />
+      <Skill   
+          emoji="👍"
+          skill="React.Js"
+          color={{ backgroundColor: "yellow" }}/>
+      <Skill 
+          emoji="✨"
+          skill="tailwind CSS"
+          color={{ backgroundColor: "green" }}/>
+      
+      <Skill 
+          emoji="👍"
+          skill="Git and Github"
+          color={{ backgroundColor: "orange" }}/>
     </div>
   );
+}
+
+function Skill({skill, color,emoji}){
+  return (
+    <p className="skill" style={color}>
+    {skill}
+    {emoji}
+  </p>
+  )
 }
